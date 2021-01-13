4d9c5afcb22d82b04fd9f9659c60e78468f140c4
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2021-01-13 16:48:03 +0100

    Start ex3

--- IMAC/TP1/Ex3/src/student.cu
+++ IMAC/TP1/Ex3/src/student.cu
@@ -12,6 +12,12 @@
 
 namespace IMAC
 {
+	__global__ void sepiaImageCUDA(const int width, const int height, const unsigned char* dev_input, unsigned char* dev_output)
+	{
+		int idx = blockIdx.x * blockDim.x + threadIdx.x;
+		dev_output[idx] = dev_input[idx];
+	}
+
 	void studentJob(const std::vector<uchar> &input, const uint width, const uint height, std::vector<uchar> &output)
 	{
 		ChronoGPU chrGPU;
@@ -19,7 +25,23 @@ namespace IMAC
 		// 2 arrays for GPU
 		uchar *dev_input = NULL;
 		uchar *dev_output = NULL;
+
+		// Allocate memory
+		const size_t bytes = width * height * 3 * sizeof(uchar);
+		cudaMalloc(&dev_input, bytes);
+		cudaMalloc(&dev_output, bytes);
 		
-		/// TODOOOOOOOOOOOOOO
+		// Copy data from host to device
+		cudaMemcpy(dev_input, input.data(), bytes, cudaMemcpyHostToDevice);
+
+		// Launch kernel
+		sepiaImageCUDA<<<1, 256>>>(width, height, dev_input, dev_output);
+
+		// Copy data from device to host
+		cudaMemcpy(output.data(), dev_output, bytes, cudaMemcpyDeviceToHost);
+
+		// Free memory
+		cudaFree(dev_input);
+		cudaFree(dev_output);
 	}
 }
