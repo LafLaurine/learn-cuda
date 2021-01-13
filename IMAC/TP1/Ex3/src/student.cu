777e0da64f477328f3c104c737fb753c34adc2b5
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2021-01-13 17:35:44 +0100

    Start sepia

--- IMAC/TP1/Ex3/src/student.cu
+++ IMAC/TP1/Ex3/src/student.cu
@@ -14,9 +14,19 @@ namespace IMAC
 {
 	__global__ void sepiaImageCUDA(const int width, const int height, const unsigned char* dev_input, unsigned char* dev_output)
 	{
-		int idx = blockIdx.x * blockDim.x + threadIdx.x;
-		dev_output[idx] = dev_input[idx];
-	}
+		uint x = (blockIdx.x * blockDim.x) + threadIdx.x;
+		uint y = (blockIdx.y * blockDim.y) + threadIdx.y;
+		uint idx = y * width + x;
+		if (idx < width * height * 3)
+		{
+			const unsigned char r = dev_input[idx];
+			const unsigned char v = dev_input[idx + 1];
+			const unsigned char b = dev_input[idx + 2];
+			dev_output[idx] = min(255.f, r * 0.393 + v * 0.769 + b * 0.189);
+			dev_output[idx + 1] = min(255.f, r * 0.349 + v * 0.686 + b * 0.168);
+			dev_output[idx + 2] = min(255.f, r * 0.272 + v * 0.534 + b * 0.131);
+		}
+	} 
 
 	void studentJob(const std::vector<uchar> &input, const uint width, const uint height, std::vector<uchar> &output)
 	{
@@ -35,7 +45,7 @@ namespace IMAC
 		cudaMemcpy(dev_input, input.data(), bytes, cudaMemcpyHostToDevice);
 
 		// Launch kernel
-		int blocks = (width * height * 3) / 512;
+		int blocks = (width * height) / 512;
 		sepiaImageCUDA<<<blocks, 512>>>(width, height, dev_input, dev_output);
 
 		// Copy data from device to host
