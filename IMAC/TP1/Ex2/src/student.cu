33690dde0b955fcbd1124888bc3d7dd8a58e6e0c
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2021-01-13 15:41:01 +0100

    Exo1.1

--- IMAC/TP1/Ex2/src/student.cu
+++ IMAC/TP1/Ex2/src/student.cu
@@ -7,6 +7,11 @@
 * Author: Maxime MARIA
 */
 
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <vector>
+#include <iostream>
+
 #include "student.hpp"
 #include "chronoGPU.hpp"
 
@@ -14,7 +19,7 @@ namespace IMAC
 {
 	__global__ void sumArraysCUDA(const int n, const int *const dev_a, const int *const dev_b, int *const dev_res)
 	{
-		/// TODO
+		dev_res[threadIdx.x] = dev_a[threadIdx.x] + dev_b[threadIdx.x];
 	}
 
     void studentJob(const int size, const int *const a, const int *const b, int *const res)
@@ -32,22 +37,27 @@ namespace IMAC
 					<< ( ( 3 * bytes ) >> 20 ) << " MB on Device" << std::endl;
 		chrGPU.start();
 		
-		/// TODO
+		cudaMalloc(&dev_a, bytes);
+		cudaMalloc(&dev_b, bytes);
+		cudaMalloc(&dev_res, bytes);
 		
 		chrGPU.stop();
 		std::cout 	<< "-> Done : " << chrGPU.elapsedTime() << " ms" << std::endl << std::endl;
 
 		// Copy data from host to device (input arrays) 
-		/// TODO
+		cudaMemcpy(dev_a, a, bytes, cudaMemcpyHostToDevice);
+		cudaMemcpy(dev_b, b, bytes, cudaMemcpyHostToDevice);
 
 		// Launch kernel
-		/// TODO
+		sumArraysCUDA<<<1, 256>>>(size, dev_a, dev_b, dev_res);
 
 		// Copy data from device to host (output array)  
-		/// TODO
+		cudaMemcpy(res, dev_res, bytes, cudaMemcpyDeviceToHost);
 
 		// Free arrays on device
-		/// TODO
+		cudaFree(dev_a);
+		cudaFree(dev_b);
+		cudaFree(dev_res);
 	}
 }
 
