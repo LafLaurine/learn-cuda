d50f88d43db8c3a0080f44b26d494f2eddc67d26
Laurine <laurine.lafontaine@outlook.fr>
2021-01-16 18:07:33 +0100

    [TP1] - Exo4 need to clean pointer problem

--- IMAC/TP1/Ex4/src/addMatrix.cu
+++ IMAC/TP1/Ex4/src/addMatrix.cu
@@ -7,36 +7,36 @@
 #include "chronoGPU.hpp"
 #include "common.hpp"
 
-#define WIDTH 10
-#define HEIGHT 10
+#define WIDTH 2
+#define HEIGHT 2
 #define SIZE HEIGHT*WIDTH
 
 void createMatrix(int m[SIZE])
 {
     for (int i = 0; i < SIZE; i++) {
-        m[i] = 1;
+        m[i] = rand()%100;
     }
 }
 
 void matricesSum(const int m1[SIZE], const int m2[SIZE])
 {   
     int s[SIZE] = {0};
-    std::cout << "\n Output of the sum : " << std::endl;
+    std::cout << "Output of the sum : " << std::endl;
 
     for (int ix = 0; ix < WIDTH; ix++) { 
         for (int iy = 0; iy < HEIGHT; iy++) {
             s[iy*WIDTH + ix] = m1[iy*WIDTH + ix] + m2[iy*WIDTH + ix];
-            std::cout << s[iy*WIDTH + ix] << " ";
+            std::cout << s[iy*WIDTH + ix] << std::endl;
         }
     }
 }
 
-__global__ void matSum(const int* dev_a[SIZE], const int* dev_b[SIZE], int* dev_res[SIZE]){
+__global__ void matSum(const int dev_a[SIZE], const int dev_b[SIZE], int dev_res[SIZE]){
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int idy = blockIdx.y * blockDim.y + threadIdx.y;
     int index = (idy * WIDTH + idx);
     if (idx < WIDTH && idy < HEIGHT){
-        *dev_res[index] = *dev_a[index] + *dev_b[index];
+        dev_res[index] = dev_a[index] + dev_b[index];
     }
 }
 
@@ -51,27 +51,23 @@ void GPUCompute(const int a[SIZE], const int b[SIZE], int res[SIZE]) {
 
     // Allocate arrays on device (input and ouput)
     const size_t bytes = SIZE*sizeof(int);
-    std::cout 	<< "\n Allocating input" 
-                << ( ( 3 * bytes ) >> 20 ) << " MB on Device" << std::endl;
 
     cudaMalloc((void**)&dev_a, bytes);
     cudaMalloc((void**)&dev_b, bytes);
     cudaMalloc((void**)&dev_res, bytes);
 
     //The error lays here
-    HANDLE_ERROR(cudaMemcpy(dev_a, &a, bytes, cudaMemcpyHostToDevice));
-    HANDLE_ERROR(cudaMemcpy(dev_b, &b, bytes, cudaMemcpyHostToDevice));
+    cudaMemcpy(dev_a, &a, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_b, &b, bytes, cudaMemcpyHostToDevice);
         
     int numBlocks = 1;
-    dim3 threadsPerBlock(WIDTH,HEIGHT);
-
-    matSum<<<numBlocks, threadsPerBlock>>>(dev_a,dev_b,dev_res);
-
+    dim3 threadsPerBlock(WIDTH*HEIGHT,HEIGHT*WIDTH);
+    matSum<<<numBlocks, threadsPerBlock>>>(*dev_a,*dev_b,*dev_res);
     cudaDeviceSynchronize();
 
-    HANDLE_ERROR(cudaMemcpy(&res, dev_res, bytes, cudaMemcpyDeviceToHost));
+    cudaMemcpy(&res, dev_res, bytes, cudaMemcpyDeviceToHost);
 
-    std::cout << res[0] << std::endl;
+    std::cout << *res << std::endl;
 
     cudaFree(dev_res);
     cudaFree(dev_b);
