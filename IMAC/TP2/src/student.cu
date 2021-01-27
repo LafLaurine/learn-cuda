8cb692107fa23fa25e586c2224e1589121bd5efe
LafLaurine <laurine.lafontaine@outlook.fr>
2021-01-27 17:38:22 +0100

    [TP2] - Ex3

--- IMAC/TP2/src/student.cu
+++ IMAC/TP2/src/student.cu
@@ -101,7 +101,7 @@ namespace IMAC
 		}
 	}
 
-	__device__ __constant__ float dev_matConv[KERNEL_SIZE];
+	__device__ __constant__ float dev_matConv[KERNEL_SIZE * KERNEL_SIZE];
 
 	__global__ void applyConvolutionv2(const unsigned char* dev_input, const uint imgWidth, const uint imgHeight,  const uint matSize, float* dev_matConvn, unsigned char* dev_output)
 	{
@@ -144,6 +144,49 @@ namespace IMAC
 		}
 	}
 
+	texture<uchar> texRef;
+
+	__global__ void applyConvolutionv3(const unsigned char* dev_input, const uint imgWidth, const uint imgHeight,  const uint matSize, float* dev_matConvn, unsigned char* dev_output)
+	{
+		unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+		unsigned int idy = blockIdx.y * blockDim.y + threadIdx.y;
+		int index = (idy * imgWidth + idx) * 4;
+		if (idx < imgWidth && idy < imgHeight){
+			float3 sum = make_float3(0.f,0.f,0.f);
+			for (uint j = 0; j < matSize; j++) 
+			{
+				for (uint i = 0; i < matSize; i++) 
+				{
+					int dX = idx + i - matSize / 2;
+					int dY = idy + j - matSize / 2;
+
+					// Handle borders
+					if (dX < 0) 
+						dX = 0;
+
+					if (dX >= imgWidth) 
+						dX = imgWidth - 1;
+
+					if (dY < 0) 
+						dY = 0;
+
+					if (dY >= imgHeight) 
+						dY = imgHeight - 1;
+					
+					const int idMat = j * matSize + i;
+					const int idPixel = (dY * imgWidth + dX) * 4;
+					sum.x += (float)tex1Dfetch(texRef,idPixel) * dev_matConv[idMat];
+					sum.y += (float)tex1Dfetch(texRef,idPixel+1) * dev_matConv[idMat];
+					sum.z += (float)tex1Dfetch(texRef,idPixel+2) * dev_matConv[idMat];
+				}
+			}
+			dev_output[index] = (uchar)max(0.f,min(255.f,sum.x));
+			dev_output[index+1] = (uchar)max(0.f,min(255.f,sum.y));
+			dev_output[index+2] = (uchar)max(0.f,min(255.f,sum.z));
+			dev_output[index+3] = 255;
+		}
+	}
+
 
     void studentJob(const std::vector<uchar4> &inputImg, // Input image
 					const uint imgWidth, const uint imgHeight, // Image size
@@ -159,7 +202,7 @@ namespace IMAC
 		uchar* dev_input = NULL;
 		uchar* dev_output = NULL;	
 		//float* dev_matConv = NULL;
-
+		chrGPU.start();
 		dim3 threadsPerBlock(BLOCK_SIZE, BLOCK_SIZE);
 		dim3 numBlocks(ceil((float)imgWidth / threadsPerBlock.x), ceil((float)imgHeight/threadsPerBlock.y));
 
@@ -167,17 +210,20 @@ namespace IMAC
 		HANDLE_ERROR(cudaMalloc((void**)&dev_input, imgWidth * imgHeight * 4 * sizeof(uchar)));
 		HANDLE_ERROR(cudaMalloc((void**)&dev_output, imgWidth * imgHeight * 4 * sizeof(uchar)));
 		//HANDLE_ERROR(cudaMalloc((void**)&dev_matConv, matSize * matSize * sizeof(float)));
-		cudaMemcpyToSymbol(dev_matConv, matConv.data(), matSize*matSize*sizeof(float));
+		
+		HANDLE_ERROR(cudaMemcpyToSymbol(dev_matConv, matConv.data(), matSize*matSize*sizeof(float)));
 		
 		// Copy data from host to device (input arrays) 
 		HANDLE_ERROR(cudaMemcpy(dev_input, inputImg.data(), (imgWidth * imgHeight * 4) * sizeof(uchar), cudaMemcpyHostToDevice));
 		//HANDLE_ERROR(cudaMemcpy(dev_matConv, matConv.data(), matSize * matSize * sizeof(float), cudaMemcpyHostToDevice));
 
-		chrGPU.start();
+		HANDLE_ERROR(cudaBindTexture(NULL, texRef, dev_input,  imgWidth * imgHeight * 4 * sizeof(uchar)));
+
 		//launch kernel
 		//applyConvolution<<<numBlocks,threadsPerBlock>>>(dev_input,imgWidth,imgHeight,matSize,dev_matConv,dev_output);
-		applyConvolutionv2<<<numBlocks,threadsPerBlock>>>(dev_input,imgWidth,imgHeight,matSize,dev_matConv,dev_output);
-		
+		//applyConvolutionv2<<<numBlocks,threadsPerBlock>>>(dev_input,imgWidth,imgHeight,matSize,dev_matConv,dev_output);
+		applyConvolutionv3<<<numBlocks,threadsPerBlock>>>(dev_input,imgWidth,imgHeight,matSize,dev_matConv,dev_output);
+
 		chrGPU.stop();
 		std::cout << "-> Done : " << chrGPU.elapsedTime() << " ms" << std::endl << std::endl;
 
@@ -185,9 +231,10 @@ namespace IMAC
  		// Copy data from device to host (output array)
 		HANDLE_ERROR(cudaMemcpy(output.data(), dev_output, (imgWidth * imgHeight * 4) * sizeof(uchar), cudaMemcpyDeviceToHost));
 
+		HANDLE_ERROR(cudaUnbindTexture(texRef));
 		// Free arrays on device
-		cudaFree(dev_output);
+		HANDLE_ERROR(cudaFree(dev_output));
 		//cudaFree(dev_matConv);
-		cudaFree(dev_input);
+		HANDLE_ERROR(cudaFree(dev_input));
 	}
 }
