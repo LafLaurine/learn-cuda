e524d967c5de8626944de96e6e88739b007b60f9
LafLaurine <laurine.lafontaine@outlook.fr>
2021-01-28 15:41:12 +0100

    [TP2] - Almost done with ex4

--- IMAC/TP2/src/student.cu
+++ IMAC/TP2/src/student.cu
@@ -145,6 +145,7 @@ namespace IMAC
 	}
 
 	texture<uchar> texRef;
+	texture<uchar,2> tex2DRef;
 
 	__global__ void applyConvolutionv3(const unsigned char* dev_input, const uint imgWidth, const uint imgHeight,  const uint matSize, float* dev_matConvn, unsigned char* dev_output)
 	{
@@ -187,6 +188,47 @@ namespace IMAC
 		}
 	}
 
+	__global__ void applyConvolutionv4(const unsigned char* dev_input, const uint imgWidth, const uint imgHeight,  const uint matSize, float* dev_matConvn, unsigned char* dev_output)
+	{
+		unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+		unsigned int idy = blockIdx.y * blockDim.y + threadIdx.y;
+		int index = (idy * imgWidth + idx) * 4;
+		if (idx < imgWidth && idy < imgHeight){
+			float3 sum = make_float3(0.f,0.f,0.f);
+			for (uint j = 0; j < matSize; j++) 
+			{
+				for (uint i = 0; i < matSize; i++) 
+				{
+					int dX = idx + i - matSize / 2;
+					int dY = idy + j - matSize / 2;
+
+					// Handle borders
+					if (dX < 0) 
+						dX = 0;
+
+					if (dX >= imgWidth) 
+						dX = imgWidth - 1;
+
+					if (dY < 0) 
+						dY = 0;
+
+					if (dY >= imgHeight) 
+						dY = imgHeight - 1;
+					
+					const int idMat = j * matSize + i;
+					const int idPixel = (dY * imgWidth + dX) * 4;
+					sum.x += (float)tex2D(tex2DRef,dY * 4,dX * 4) * dev_matConv[idMat];
+					sum.y += (float)tex2D(tex2DRef,(dY * 4) + 1, (dX*4)+1) * dev_matConv[idMat];
+					sum.z += (float)tex2D(tex2DRef,(dY * 4) + 2, (dX*4)+2) * dev_matConv[idMat];
+				}
+			}
+			dev_output[index] = (uchar)max(0.f,min(255.f,sum.x));
+			dev_output[index+1] = (uchar)max(0.f,min(255.f,sum.y));
+			dev_output[index+2] = (uchar)max(0.f,min(255.f,sum.z));
+			dev_output[index+3] = 255;
+		}
+	}
+
 
     void studentJob(const std::vector<uchar4> &inputImg, // Input image
 					const uint imgWidth, const uint imgHeight, // Image size
@@ -201,6 +243,7 @@ namespace IMAC
 		// 3 arrays for GPU
 		uchar* dev_input = NULL;
 		uchar* dev_output = NULL;	
+		size_t pitch;
 		//float* dev_matConv = NULL;
 		chrGPU.start();
 		dim3 threadsPerBlock(BLOCK_SIZE, BLOCK_SIZE);
@@ -210,19 +253,23 @@ namespace IMAC
 		HANDLE_ERROR(cudaMalloc((void**)&dev_input, imgWidth * imgHeight * 4 * sizeof(uchar)));
 		HANDLE_ERROR(cudaMalloc((void**)&dev_output, imgWidth * imgHeight * 4 * sizeof(uchar)));
 		//HANDLE_ERROR(cudaMalloc((void**)&dev_matConv, matSize * matSize * sizeof(float)));
-		
+		HANDLE_ERROR(cudaMallocPitch((void**)&dev_input, &pitch, (imgWidth * 4) * sizeof(uchar), (imgHeight * 4) * sizeof(uchar)));
+
 		HANDLE_ERROR(cudaMemcpyToSymbol(dev_matConv, matConv.data(), matSize*matSize*sizeof(float)));
 		
 		// Copy data from host to device (input arrays) 
-		HANDLE_ERROR(cudaMemcpy(dev_input, inputImg.data(), (imgWidth * imgHeight * 4) * sizeof(uchar), cudaMemcpyHostToDevice));
+		//HANDLE_ERROR(cudaMemcpy(dev_input, inputImg.data(), (imgWidth * imgHeight * 4) * sizeof(uchar), cudaMemcpyHostToDevice));
 		//HANDLE_ERROR(cudaMemcpy(dev_matConv, matConv.data(), matSize * matSize * sizeof(float), cudaMemcpyHostToDevice));
+		HANDLE_ERROR(cudaMemcpy2D(dev_input, pitch, inputImg.data(), pitch, imgWidth, imgHeight, cudaMemcpyHostToDevice));
 
-		HANDLE_ERROR(cudaBindTexture(NULL, texRef, dev_input,  imgWidth * imgHeight * 4 * sizeof(uchar)));
+		//HANDLE_ERROR(cudaBindTexture(NULL, texRef, dev_input,  imgWidth * imgHeight * 4 * sizeof(uchar)));
+		HANDLE_ERROR(cudaBindTexture2D(NULL, tex2DRef, dev_input,  imgWidth * 4 * sizeof(uchar), imgHeight * 4 * sizeof(uchar), pitch));
 
 		//launch kernel
 		//applyConvolution<<<numBlocks,threadsPerBlock>>>(dev_input,imgWidth,imgHeight,matSize,dev_matConv,dev_output);
 		//applyConvolutionv2<<<numBlocks,threadsPerBlock>>>(dev_input,imgWidth,imgHeight,matSize,dev_matConv,dev_output);
-		applyConvolutionv3<<<numBlocks,threadsPerBlock>>>(dev_input,imgWidth,imgHeight,matSize,dev_matConv,dev_output);
+		//applyConvolutionv3<<<numBlocks,threadsPerBlock>>>(dev_input,imgWidth,imgHeight,matSize,dev_matConv,dev_output);
+		applyConvolutionv4<<<numBlocks,threadsPerBlock>>>(dev_input,imgWidth,imgHeight,matSize,dev_matConv,dev_output);
 
 		chrGPU.stop();
 		std::cout << "-> Done : " << chrGPU.elapsedTime() << " ms" << std::endl << std::endl;
