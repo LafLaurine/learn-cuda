9382e7aa7353a8727cec9232a5e66ceae71045ee
Guillaume Haerinck <haerinck.guillaume@gmail.com>
2021-02-02 10:58:36 +0100

    [TP1] Ex4

--- IMAC/TP1/Ex4/main.cu
+++ IMAC/TP1/Ex4/main.cu
@@ -3,83 +3,82 @@
 #include <device_launch_parameters.h>
 
 #include <iostream>
-#include<time.h>
 
-#define WIDTH 2
-#define HEIGHT 2
-#define SIZE HEIGHT*WIDTH
-
-void createMatrix(int m[SIZE])
-{
-    for (int i = 0; i < SIZE; i++) {
-        m[i] = rand()%100;
-    }
-}
-
-void matricesSum(const int m1[SIZE], const int m2[SIZE])
+void CPUMatrixSum(const int* m1, const int* m2, const int height, const int width, int* res)
 {   
-    int s[SIZE] = {0};
-    std::cout << "Output of the sum : ";
-
-    for (int ix = 0; ix < WIDTH; ix++) { 
-        for (int iy = 0; iy < HEIGHT; iy++) {
-            s[iy*WIDTH + ix] = m1[iy*WIDTH + ix] + m2[iy*WIDTH + ix];
-            std::cout << s[iy*WIDTH + ix] << " ";
+    for (int iy = 0; iy < height; iy++) {
+        for (int ix = 0; ix < width; ix++) {
+            const int index = iy*width + ix;
+            res[index] = m1[index] + m2[index];
         }
     }
 }
 
-__global__ void matSum(int* dev_a, int* dev_b, int* dev_res){
+__global__ void GPUMatrixSum(const int* m1, const int* m2, const int height, const int width, int* res)
+{
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int idy = blockIdx.y * blockDim.y + threadIdx.y;
-    int index = (idy * WIDTH + idx);
-    if (idx < WIDTH && idy < HEIGHT){
-        dev_res[index] = dev_a[index] + dev_b[index];
+    int index = (idy * width + idx);
+    if (idx < width && idy < height){
+        res[index] = m1[index] + m2[index];
     }
 }
 
-void CPUCompute(const int m1[SIZE], const int m2[SIZE]) {
-    matricesSum(m1,m2);
-}
-
-void GPUCompute(const int* a, const int* b, int* res) {
-    int* dev_a = NULL;
-    int* dev_b = NULL;
-    int* dev_res = NULL;
+void GPUCompute(const int* m1, const int* m2, const int height, const int width, int* res) 
+{
+    int* dev_m1 = nullptr;
+    int* dev_m2 = nullptr;
+    int* dev_res = nullptr;
 
     // Allocate arrays on device (input and ouput)
-    const size_t bytes = SIZE*sizeof(int);
-
-    cudaMalloc((void**)&dev_a, bytes);
-    cudaMalloc((void**)&dev_b, bytes);
+    const size_t bytes = width * height *sizeof(int);
+    cudaMalloc((void**)&dev_m1, bytes);
+    cudaMalloc((void**)&dev_m2, bytes);
     cudaMalloc((void**)&dev_res, bytes);
 
-    //The error lays here
-    cudaMemcpy(dev_a, a, bytes, cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_b, b, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_m1, m1, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_m2, m2, bytes, cudaMemcpyHostToDevice);
         
     int numBlocks = 1;
-    dim3 threadsPerBlock(WIDTH*HEIGHT,HEIGHT*WIDTH);
-    matSum<<<numBlocks, threadsPerBlock>>>(dev_a,dev_b,dev_res);
-    cudaDeviceSynchronize();
+    dim3 threadsPerBlock(width*height, width*height);
+    GPUMatrixSum<<<numBlocks, threadsPerBlock>>>(dev_m1, dev_m2, height, width, dev_res);
 
     cudaMemcpy(res, dev_res, bytes, cudaMemcpyDeviceToHost);
     
     cudaFree(dev_res);
-    cudaFree(dev_b);
-    cudaFree(dev_a);
+    cudaFree(dev_m2);
+    cudaFree(dev_m1);
+}
 
+void DisplayMatrix(int* m, const int height, const int width)
+{
+    for (int ix = 0; ix < width; ix++) { 
+        for (int iy = 0; iy < height; iy++) {
+            std::cout << m[iy*width + ix] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
 
 int main()
-{   
-    srand(static_cast<unsigned>(time(0)));
-    int m1[SIZE];
-    createMatrix(m1);
-    int m2[SIZE];
-    createMatrix(m2);
-    int mres[SIZE] = {0};
-    CPUCompute(m1,m2);
-    GPUCompute(m1,m2,mres);
+{
+    const int width = 2;
+    const int height = 2;
+
+    int m1[width * height];
+    std::fill(m1, m1 + width * height, 1);
+
+    int m2[width * height];
+    std::fill(m2, m2 + width * height, 2);
+
+    std::cout << "CPU compute :" << std::endl;
+    int mres[width * height] = {0};
+    CPUMatrixSum(m1, m2, height, width, mres);
+    DisplayMatrix(mres, width, height);
+
+    std::cout << "GPU compute :" << std::endl;
+    std::fill(mres, mres + width * height, 0);
+    GPUCompute(m1, m2, height, width, mres);
+    DisplayMatrix(mres, width, height);
 	return 0;
 }
